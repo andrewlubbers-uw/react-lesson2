1a7dc49662b09e3fd9345e28bc0fcb9cb31e5f49
sudduth <gsudduth@gmail.com>
2015-04-10 07:22:07 -0700

    Removed css loader from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@
 module.exports = {
     module: {
         loaders: [
-            {test: /\.css$/, loader: "css" },
             {test: /\.(jsx|js)$/, loader: 'jsx-loader' },
             {test: /\.(png|jpg|gif)$/, loader: 'url-loader?limit=8192'}
         ]
